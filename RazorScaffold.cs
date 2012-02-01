4e17e4c6951cdd6ea565a98e986265ce47bcbefb
jracabado <jracabado@gmail.com>
2012-02-01 08:28:08 +0000

    First commit, 0.01a

---
+++ RazorScaffold.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.WebPages;
+using System.Collections.Generic;
+using umbraco.MacroEngines;
+
+namespace GoingleUmbraco
+{
+    public static class RazorScaffold
+    {
+        public static HelperResult Render(DynamicNode node, string mode = "")
+        {
+            return RazorScaffoldCore.Instance.ApplyTemplate(node, mode);
+        }
+
+        public static HelperResult Render(List<DynamicNode> nodeList, string mode = "")
+        {
+            if (nodeList == null)
+                throw new ArgumentException("Empty node list.", "nodeList");
+
+            var helperList = new List<HelperResult>();
+            foreach (var node in nodeList)
+            {
+                helperList.Add(RazorScaffoldCore.Instance.ApplyTemplate(node, mode));
+            }
+
+            return new HelperResult(tw => { foreach (var hr in helperList) { hr.WriteTo(tw); } }); 
+        }
+    }
+}
